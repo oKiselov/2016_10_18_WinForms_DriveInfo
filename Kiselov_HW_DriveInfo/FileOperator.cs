cad0092f2a8aaf02c7f36c3c2f5a9def567b941d
oKiselov <dp.prk.kiselov@gmail.com>
2016-10-19 02:22:06 +0300

    Написать программу, которая при подключении нового диска осуществляет поиск всех текстовых файлов и копирование к себе в созданный каталог, имя которого должно соответствовать метке подключенного диска. Определение метки диска вспоминайте DriveInfo. А поиск и копирование файлов это уже консольный C#. Интерфес придумайте на свой вкус. Обязательно проверки и комментарии.

---
+++ Kiselov_HW_DriveInfo/FileOperator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kiselov_HW_DriveInfo
+{
+    class FileOperator
+    {
+        // available drive-devices counter 
+        private int _intDriveCounter;
+
+        public FileOperator()
+        {
+            _intDriveCounter = 0;
+        }
+
+        /// <summary>
+        /// Method which search your directory for txt files 
+        /// Returns collection of pathes to such files 
+        /// </summary>
+        /// <param name="strPath"></param>
+        /// <returns></returns>
+        public List<string> TxtBrowser(string strPath)
+        {
+            List<string> listTxt =
+                new List<string>(Directory.EnumerateFiles(strPath, "*.txt", SearchOption.TopDirectoryOnly));
+            listTxt.Insert(0, DateTime.Now.ToString());
+            _intDriveCounter++;
+            return listTxt;
+        }
+
+        /// <summary>
+        /// Method writes all pathes of txt files to file TextFilesLog.txt
+        /// </summary>
+        /// <param name="listTxt"></param>
+        public void ToFile(List<string> listTxt)
+        {
+            using (FileStream fs = new FileStream("TextFilesLog.txt", FileMode.Append, FileAccess.Write))
+            {
+                for (int i = 0; i < listTxt.Count; i++)
+                {
+                    byte[] arrBytes = Encoding.Unicode.GetBytes(listTxt[i] += "\r\n");
+                    fs.Write(arrBytes, 0, arrBytes.Length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method reads all pathes of txt files from the file TextFilesLog.txt
+        /// </summary>
+        /// <param name="strPath"></param>
+        public void FromFile(string strPath = "TextFilesLog.txt")
+        {
+            byte[] arrBytesTemp = new byte[4096];
+            using (FileStream fs = new FileStream(strPath, FileMode.Open, FileAccess.Read))
+            {
+                int iCount = 0;
+                while (iCount < fs.Length)
+                {
+                    int iRead = fs.Read(arrBytesTemp, 0, arrBytesTemp.Length);
+                    if (iRead == 0)
+                    {
+                        break;
+                    }
+                    if (iRead != 4096)
+                    {
+                        Array.Resize(ref arrBytesTemp, iRead);
+                    }
+                    string strTemp = Encoding.Unicode.GetString(arrBytesTemp);
+                    Console.WriteLine(strTemp);
+                    iCount += (int)iRead;
+                }
+            }
+        }
+
+        public int DriveCounter
+        {
+            get { return _intDriveCounter; }
+        }
+    }
+}
---
+++ Kiselov_HW_DriveInfo/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kiselov_HW_DriveInfo
+{
+    public partial class Form1 : Form
+    {
+        // object for operations with txt files 
+        private FileOperator _fileOperator;
+        // list of DriveInfo about current state of devices 
+        private List<DriveInfo> _listOfDriveInfo;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // initialization of object 
+            _fileOperator = new FileOperator();
+
+            // checking all removable devices 
+            _listOfDriveInfo = new List<DriveInfo>(DriveInfo.GetDrives())
+                .Where(drive => drive.DriveType == DriveType.Removable).ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// button Start 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText("Start!");
+        }
+
+        /// <summary>
+        ///  button Stop
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// overriding of windows operation 
+        /// handling messages 
+        /// </summary>
+        /// <param name="msg"></param>
+        protected override void WndProc(ref Message msg)
+        {
+            List<DriveInfo> listDriveTemp = new List<DriveInfo>();
+            const int WM_DEVICECHANGE = 0x0219;
+            const int ADD_DEVICE = 0x8000;
+            const int REMOVE_DEVICE = 0x8004;
+            if (msg.Msg == WM_DEVICECHANGE)
+            {
+                switch ((int)msg.WParam)
+                {
+                    case ADD_DEVICE:
+                        // check that disc is removable 
+                        listDriveTemp = new List<DriveInfo>(DriveInfo.GetDrives())
+                            .Where(drive =>drive.DriveType==DriveType.Removable).ToList();
+                        // check that amount of removable discs has been changed 
+                        if (listDriveTemp.Count > _listOfDriveInfo.Count)
+                        {
+                            // search what disc is new 
+                            for (int i = 0; i < listDriveTemp.Count; i++)
+                            {
+                                if (!_listOfDriveInfo.Any(drive => drive.Name == listDriveTemp[i].Name))
+                                {
+                                    _listOfDriveInfo.Add(listDriveTemp[i]);
+                                }
+                            }
+
+                            DriveInfo targetDrive = _listOfDriveInfo[_listOfDriveInfo.Count - 1];
+                            // check that disc is ready for writing 
+                            if (targetDrive.IsReady)
+                            {
+                                List<string> listTmp = _fileOperator.TxtBrowser(targetDrive.Name);
+                                _fileOperator.ToFile(listTmp);
+                                // output to the text box 
+                                foreach (var VARIABLE in listTmp)
+                                {
+                                    richTextBox1.AppendText(VARIABLE);
+                                }
+                                
+                            }
+                        }
+                        break;
+
+                    case REMOVE_DEVICE:
+                        // set new list of discs after remove 
+                        _listOfDriveInfo = new List<DriveInfo>(DriveInfo.GetDrives())
+                            .Where(drive =>drive.DriveType==DriveType.Removable).ToList();                     
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+            // transfer to main function 
+            base.WndProc(ref msg);
+        }
+
+        /// <summary>
+        /// TextBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
